c6afce6511640a533f24f6061a18eda32ce21eab
Stefan Savić <69756458+stevacode@users.noreply.github.com>
2026-08-11 17:39:24 +0200

    Initial commit

---
+++ Trees.cpp
@@ -0,0 +1,88 @@
+﻿// Trees.cpp : Binary tree
+//
+#include "Trees.h"
+
+
+TreeNode* root = nullptr;
+
+void Treecreate()
+{
+	TreeNode* p, * t;
+	int x;
+	Queue q;
+	create(&q, 100);
+	cout << "Enter root value: ";
+	cin >> x;
+	root = new TreeNode;
+	root->data = x;
+	root->left = root->right = nullptr;
+	enqueue(&q, root);
+	while (!isEmpty(&q))
+	{
+		p = dequeue(&q);
+		cout << "Enter left child of " << p->data << ": ";
+		cin >> x;
+		if (x != -1)
+		{
+			t = new TreeNode;
+			t->data = x;
+			t->left = t->right = nullptr;
+			p->left = t;
+			enqueue(&q, t);
+		}
+		cout << "Enter right child of " << p->data << ": ";
+		cin >> x;
+		if (x != -1)
+		{
+			t = new TreeNode;
+			t->data = x;
+			t->left = t->right = nullptr;
+			p->right = t;
+			enqueue(&q, t);
+		}
+	}
+}
+
+void preorder(TreeNode* p)
+{
+	if (p)
+	{
+		cout << p->data << " ";
+		preorder(p->left);
+		preorder(p->right);
+	}
+}
+
+void inorder(TreeNode* p)
+{
+	if (p)
+	{
+		inorder(p->left);
+		cout << p->data << " ";
+		inorder(p->right);
+	}
+}
+
+void postorder(TreeNode* p)
+{
+	if (p)
+	{
+		postorder(p->left);
+		postorder(p->right);
+		cout << p->data << " ";
+	}
+}
+
+
+int main()
+{
+	Treecreate();
+	preorder(root);
+	cout << endl;
+	postorder(root);
+	cout << endl;
+	inorder(root);
+	cout << endl;
+
+	return 0;
+}
---
+++ Trees.h
@@ -0,0 +1,59 @@
+﻿// Trees.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+struct TreeNode {
+	int data;
+	TreeNode* left;
+	TreeNode* right;
+};
+
+struct Queue {
+	int size;
+	int front;
+	int rear;
+	TreeNode** Q;
+};
+
+void create(Queue* q, int size)
+{
+	q->size = size;
+	q->front = q->rear = 0;
+	q->Q  = new TreeNode*[q->size];
+}
+
+void enqueue(Queue* q, TreeNode* x)
+{
+	if ((q->rear + 1) % q->size == q->front)
+		cout << "Queue is full" << endl;
+	else
+	{
+		q->rear = (q->rear + 1) % q->size;
+		q->Q[q->rear] = x;
+	}
+}
+
+TreeNode* dequeue(Queue* q)
+{
+	TreeNode* x = nullptr;
+	if (q->front == q->rear)
+		cout << "Queue is empty" << endl;
+	else
+	{
+		q->front = (q->front + 1) % q->size;
+		x = q->Q[q->front];
+	}
+	return x;
+}
+
+int isEmpty(Queue* q)
+{
+	return q->front == q->rear;
+}
+
+
+// TODO: Reference additional headers your program requires here.
